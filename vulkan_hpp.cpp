1857a1b833a88599ef43acdf577e9303059389bd
nagaunv <naga@cavelab.cs.tsukuba.ac.jp>
2018-05-24 18:22:58 +0900

    [追加] 雛形を追加

--- vulkan_hpp.cpp
+++ vulkan_hpp.cpp
@@ -1,5 +1,22 @@
-#include <vulkan/vulkan.hpp>
+#if defined(USE_VULKAN_HPP)
+  #include <vulkan/vulkan.hpp>
+#else
+  #include <vulkan/vulkan.h>
+#endif
+
+#include <iostream>
 
 /// \file importing Vulkan-Hpp from Vulkan SDK
 
-int main() {}
+static constexpr const char* app_name = __FILE__;
+static constexpr const char* engine_name = "Vulkan.hpp";
+
+int main() {
+#if defined(USE_VULKAN_HPP)
+  std::cout << "Hello, Vulkan!" << std::endl;
+  // vulkan.hpp
+#else
+  // vulkan.h
+  std::cout << "Hello, Vulkan!" << std::endl;
+#endif
+}
